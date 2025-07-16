2ae2ab63f12599d407305e582131c0d20c5e265f
tanmay019ai <tanmaysr019@gmail.com>
2025-07-16 17:25:47 +0530

    portfolio -2

--- new-3d/src/components/Experience.jsx
+++ new-3d/src/components/Experience.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { experiences } from '../constants';
+import { SectionWrapper } from '../hoc';
+import { textVariant } from '../utils/motion';
 
 const Experience = () => {
   return (
     <div>Experience</div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- new-3d/src/constants/index.js
+++ new-3d/src/constants/index.js
@@ -46,15 +46,15 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "AI Engineer",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "UI/UX Designer",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Freelancer",
     icon: creator,
   },
 ];
