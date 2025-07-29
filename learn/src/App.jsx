38fd2a36982945f77c43ae5a298dbf194603123c
tanmay019ai <tanmaysr019@gmail.com>
2025-07-30 00:22:37 +0530

    react -SCRIPT78

---
+++ learn/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [userName, setUserName] = useState("Tanmay");
+  const [number, setNumber] = useState(0);
+
+  // Function to change the name
+  const handleChangeName = () => 
+    {
+      if (userName === "Sarthak") {
+        setUserName("Tanmay");
+        return;
+      }
+    setUserName("Sarthak");
+  };
+
+  return (
+    <div>
+      <h1>User name is {userName}</h1>
+      <button onClick={handleChangeName}>Change user name</button>
+
+      <h1>Number is {number}</h1>
+      <button id='hi' onClick={() => setNumber(number + 1)}>Increment</button>
+      <button id='ho' onClick={() => setNumber(number - 1)}>Decrement</button>
+    </div>
+
+  );
+}
+
+export default App;
